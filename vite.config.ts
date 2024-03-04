e42a2f6210b770b507ad383c886516dc2c8e47b3
fezproof <26830309+fezproof@users.noreply.github.com>
2024-03-05 01:00:35 +0800

    SSR

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { vitePlugin as remix } from "@remix-run/dev";
+import {
+  cloudflareDevProxyVitePlugin,
+  vitePlugin as remix,
+} from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [remix({ ssr: false }), tsconfigPaths()],
+  plugins: [cloudflareDevProxyVitePlugin(), remix(), tsconfigPaths()],
   server: {
     fs: {
       // Restrict files that could be served by Vite's dev server.  Accessing
